7407750ed6e53ccb05c31ef351ca7a2835b48574
Thom Barrett <thom440@yahoo.com>
2021-04-05 20:42:09 -0700

    Add project files.

---
+++ LumberManagerWebEdition/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LumberManagerWebEdition.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
