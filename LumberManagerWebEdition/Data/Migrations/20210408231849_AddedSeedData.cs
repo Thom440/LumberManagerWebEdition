2abe3afb3952eeda350924113182f490a2c2deb5
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2021-04-08 16:21:24 -0700

    Added seed data to database (#16)

---
+++ LumberManagerWebEdition/Data/Migrations/20210408231849_AddedSeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LumberManagerWebEdition.Data.Migrations
+{
+    public partial class AddedSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName" },
+                values: new object[,]
+                {
+                    { 1, "WW" },
+                    { 2, ".25" },
+                    { 3, ".40" },
+                    { 4, ".60" },
+                    { 5, "ACQ" },
+                    { 6, "CCA" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 6);
+        }
+    }
+}
