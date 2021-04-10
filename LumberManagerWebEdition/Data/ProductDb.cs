a59d4bce2cc0f7efd7c106bab3040bcca3970330
Thom Barrett <72460240+Thom440@users.noreply.github.com>
2021-04-10 13:06:29 -0700

    Created ProductDb with an async add method (#23)

---
+++ LumberManagerWebEdition/Data/ProductDb.cs
@@ -0,0 +1,23 @@
+﻿using LumberManagerWebEdition.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LumberManagerWebEdition.Data
+{
+    public static class ProductDb
+    {
+        public static async Task<Product> AddProductAsync(ApplicationDbContext _context, Product p)
+        {
+            // Add to database
+            _context.Products.Add(p);
+            for (int i = 0; i < p.Category.Count; i++)
+            {
+                _context.Categories.Attach(p.Category[i]);
+            }
+            await _context.SaveChangesAsync();
+            return p;
+        }
+    }
+}
