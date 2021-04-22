6e9bf310a386dc899594fc418f4e5547f7f2c1f5
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2021-04-21 21:14:37 -0700

    Issue#32 create category db (#35)
    * Created Category DB
    
    * Added method to grab Category by ID

---
+++ LumberManagerWebEdition/Data/CategoryDb.cs
@@ -0,0 +1,19 @@
+﻿using LumberManagerWebEdition.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LumberManagerWebEdition.Data
+{
+    public class CategoryDb
+    {
+        public static Category GetCategory(ApplicationDbContext _context, int id)
+        {
+            Category category = (from c in _context.Categories
+                                 where c.CategoryID == id
+                                 select c).Single();
+            return category;
+        } 
+    }
+}
