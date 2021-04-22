4bfabcd008bd0a4e6a8725540deb78ca7f8557db
Thom Barrett <72460240+Thom440@users.noreply.github.com>
2021-04-21 20:49:11 -0700

    Issue#22 get check boxes to work (#33)
    * Added groups to the check boxes
    
    * Added create.js
    
    * Checkboxes now properly enable and disable other checkboxes
    
    * Refactored CreateProductViewModel
    
    * Finished getting checkboxes to work

--- LumberManagerWebEdition/Controllers/ProductsController.cs
+++ LumberManagerWebEdition/Controllers/ProductsController.cs
@@ -47,7 +47,20 @@ namespace LumberManagerWebEdition.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            CreateProductViewModel createProductViewModel = new CreateProductViewModel();
+            Category category1 = new Category() { CategoryID = 1, CategoryName = "WW" };
+            Category category2 = new Category() { CategoryID = 2, CategoryName = ".25" };
+            Category category3 = new Category() { CategoryID = 3, CategoryName = ".40" };
+            Category category4 = new Category() { CategoryID = 4, CategoryName = ".60" };
+            Category category5 = new Category() { CategoryID = 5, CategoryName = "ACQ" };
+            Category category6 = new Category() { CategoryID = 6, CategoryName = "CCA" };
+            createProductViewModel.Categories.Add(category1);
+            createProductViewModel.Categories.Add(category2);
+            createProductViewModel.Categories.Add(category3);
+            createProductViewModel.Categories.Add(category4);
+            createProductViewModel.Categories.Add(category5);
+            createProductViewModel.Categories.Add(category6);
+            return View(createProductViewModel);
         }
 
         // POST: Products/Create
@@ -55,15 +68,72 @@ namespace LumberManagerWebEdition.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProductID,Height,Width,Length,OnHand,Sold")] Product product)
+        public async Task<IActionResult> Create(CreateProductViewModel createProductViewModel)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(product);
+                bool wwSelected = (Request.Form["ww"] == "WW") ? true : false;
+                bool twoFiveSelected = (Request.Form[".25"] == ".25") ? true : false;
+                bool four0Selected = (Request.Form[".40"] == ".40") ? true : false;
+                bool six0Selected = (Request.Form[".60"] == ".60") ? true : false;
+                bool acqSelected = (Request.Form["acq"] == "ACQ") ? true : false;
+                bool ccaSelected = (Request.Form["cca"] == "CCA") ? true : false;
+                if (wwSelected)
+                {
+                    Category category = new Category() { CategoryID = 1, CategoryName = "WW"};
+                    createProductViewModel.Product.Category.Add(category);
+                }
+                else
+                {
+                    if (twoFiveSelected && acqSelected)
+                    {
+                        Category category = new Category() { CategoryID = 2, CategoryName = ".25" };
+                        Category category1 = new Category() { CategoryID = 5, CategoryName = "ACQ" };
+                        createProductViewModel.Product.Category.Add(category);
+                        createProductViewModel.Product.Category.Add(category1);
+                    }
+                    else if (twoFiveSelected && ccaSelected)
+                    {
+                        Category category = new Category() { CategoryID = 2, CategoryName = ".25" };
+                        Category category1 = new Category() { CategoryID = 6, CategoryName = "CCA" };
+                        createProductViewModel.Product.Category.Add(category);
+                        createProductViewModel.Product.Category.Add(category1);
+                    }
+                    else if (four0Selected && acqSelected)
+                    {
+                        Category category = new Category() { CategoryID = 3, CategoryName = ".40" };
+                        Category category1 = new Category() { CategoryID = 5, CategoryName = "ACQ" };
+                        createProductViewModel.Product.Category.Add(category);
+                        createProductViewModel.Product.Category.Add(category1);
+                    }
+                    else if (four0Selected && ccaSelected)
+                    {
+                        Category category = new Category() { CategoryID = 3, CategoryName = ".40" };
+                        Category category1 = new Category() { CategoryID = 6, CategoryName = "CCA" };
+                        createProductViewModel.Product.Category.Add(category);
+                        createProductViewModel.Product.Category.Add(category1);
+                    }
+                    else if (six0Selected && acqSelected)
+                    {
+                        Category category = new Category() { CategoryID = 4, CategoryName = ".60" };
+                        Category category1 = new Category() { CategoryID = 5, CategoryName = "ACQ" };
+                        createProductViewModel.Product.Category.Add(category);
+                        createProductViewModel.Product.Category.Add(category1);
+                    }
+                    else if (six0Selected && ccaSelected)
+                    {
+                        Category category = new Category() { CategoryID = 4, CategoryName = ".60" };
+                        Category category1 = new Category() { CategoryID = 6, CategoryName = "CCA" };
+                        createProductViewModel.Product.Category.Add(category);
+                        createProductViewModel.Product.Category.Add(category1);
+                    }
+                }
+                await ProductDb.AddProductAsync(_context, createProductViewModel.Product);
+                //_context.Add(product);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(product);
+            return View(createProductViewModel);
         }
 
         // GET: Products/Edit/5
---
+++ LumberManagerWebEdition/Models/CreateProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LumberManagerWebEdition.Models
+{
+    public class CreateProductViewModel
+    {
+        public Product Product { get; set; }
+
+        public List<Category> Categories { get; set; } = new List<Category>();
+    }
+}
