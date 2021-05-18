fd9a9b90202b339b8f0caa9af37ef82951c115d2
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2021-05-17 20:33:26 -0700

    Issue#57 implement filters (#69)
    * Added dropdown buttons to Index to allow filtering of products
    
    * Filtering is now enabled on Product Index
    
    * Changed magic strings to already established constant variables
    
    * Implemented CustomerIndex filtration
    
    Co-authored-by: Thom Barrett <thom440@yahoo.com>

--- LumberManagerWebEdition/Data/CategoryDb.cs
+++ LumberManagerWebEdition/Data/CategoryDb.cs
@@ -1,4 +1,5 @@
 ﻿using LumberManagerWebEdition.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,33 @@ namespace LumberManagerWebEdition.Data
                                  where c.CategoryID == id
                                  select c).Single();
             return category;
-        } 
+        }
+
+        public static async Task<List<Category>> GetCategoryAsync(ApplicationDbContext _context, byte height, byte width, byte length)
+        {
+            List<Product> products = await (from p in _context.Products
+                                            where p.Height == height && p.Width == width && p.Length == length
+                                            select p).Include(nameof(Product.Category)).ToListAsync();
+
+            return (from p in products
+                    select p.Category[0]).Distinct().ToList();
+        }
+
+        public static async Task<List<Category>> GetTypeAsync(ApplicationDbContext _context, byte height, byte width, byte length, string category)
+        {
+            List<Product> products = await (from p in _context.Products
+                                            where p.Height == height && p.Width == width && p.Length == length
+                                            select p).Include(nameof(Product.Category)).ToListAsync();
+
+            List<Category> categories = new List<Category>();
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Category[0].CategoryName == category)
+                {
+                    categories.Add(products[i].Category[1]);
+                }
+            }
+            return categories;
+        }
     }
 }
--- LumberManagerWebEdition/Data/ProductDb.cs
+++ LumberManagerWebEdition/Data/ProductDb.cs
@@ -44,6 +44,108 @@ namespace LumberManagerWebEdition.Data
             return products;
         }
 
+        public static async Task<List<Product>> GetAllProductsAsync(ApplicationDbContext _context, int pageSize, int pageNum, byte height,
+                                                                                                byte width, byte length, string category, string treatmentType)
+        {
+            List<Product> products = await (from p in _context.Products
+                                            where p.Height == height && p.Width == width && p.Length == length
+                                            select p).Skip(pageSize * (pageNum - 1))
+                                               .Take(pageSize)
+                                               .Include(nameof(Product.Category))
+                                               .ToListAsync();
+            products = products.OrderBy(p => p.Height)
+                                       .ThenBy(p => p.Width)
+                                       .ThenBy(p => p.Length)
+                                       .ThenBy(p => p.Category[1].CategoryName)
+                                       .ThenBy(p => p.Category[0].CategoryName).ToList();
+
+            List<Product> newProducts = new List<Product>();
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Category[0].CategoryName == category && products[i].Category[1].CategoryName == treatmentType)
+                {
+                    newProducts.Add(products[i]);
+                }
+            }
+            return newProducts;
+        }
+
+        public static async Task<List<Product>> GetAllProductsAsync(ApplicationDbContext _context, int pageSize, int pageNum, byte height,
+                                                                                                byte width, byte length, string category)
+        {
+            List<Product> products = await (from p in _context.Products
+                                            where p.Height == height && p.Width == width && p.Length == length
+                                            select p).Skip(pageSize * (pageNum - 1))
+                                               .Take(pageSize)
+                                               .Include(nameof(Product.Category))
+                                               .ToListAsync();
+            products = products.OrderBy(p => p.Height)
+                                       .ThenBy(p => p.Width)
+                                       .ThenBy(p => p.Length)
+                                       .ThenBy(p => p.Category[1].CategoryName)
+                                       .ThenBy(p => p.Category[0].CategoryName).ToList();
+
+            List<Product> newProducts = new List<Product>();
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Category[0].CategoryName == category)
+                {
+                    newProducts.Add(products[i]);
+                }
+            }
+            return newProducts;
+        }
+
+        public static async Task<List<Product>> GetAllProductsAsync(ApplicationDbContext _context, int pageSize, int pageNum, byte height,
+                                                                                                byte width, byte length)
+        {
+            List<Product> products = await (from p in _context.Products
+                                            where p.Height == height && p.Width == width && p.Length == length
+                                            select p).Skip(pageSize * (pageNum - 1))
+                                               .Take(pageSize)
+                                               .Include(nameof(Product.Category))
+                                               .ToListAsync();
+            products = products.OrderBy(p => p.Height)
+                                       .ThenBy(p => p.Width)
+                                       .ThenBy(p => p.Length)
+                                       .ThenBy(p => p.Category[1].CategoryName)
+                                       .ThenBy(p => p.Category[0].CategoryName).ToList();
+            return products;
+        }
+
+        public static async Task<List<Product>> GetAllProductsAsync(ApplicationDbContext _context, int pageSize, int pageNum, byte height,
+                                                                                                byte width)
+        {
+            List<Product> products = await (from p in _context.Products
+                                            where p.Height == height && p.Width == width
+                                            select p).Skip(pageSize * (pageNum - 1))
+                                               .Take(pageSize)
+                                               .Include(nameof(Product.Category))
+                                               .ToListAsync();
+            products = products.OrderBy(p => p.Height)
+                                       .ThenBy(p => p.Width)
+                                       .ThenBy(p => p.Length)
+                                       .ThenBy(p => p.Category[1].CategoryName)
+                                       .ThenBy(p => p.Category[0].CategoryName).ToList();
+            return products;
+        }
+
+        public static async Task<List<Product>> GetAllProductsAsync(ApplicationDbContext _context, int pageSize, int pageNum, byte height)
+        {
+            List<Product> products = await (from p in _context.Products
+                                            where p.Height == height
+                                            select p).Skip(pageSize * (pageNum - 1))
+                                               .Take(pageSize)
+                                               .Include(nameof(Product.Category))
+                                               .ToListAsync();
+            products = products.OrderBy(p => p.Height)
+                                       .ThenBy(p => p.Width)
+                                       .ThenBy(p => p.Length)
+                                       .ThenBy(p => p.Category[1].CategoryName)
+                                       .ThenBy(p => p.Category[0].CategoryName).ToList();
+            return products;
+        }
+
         public static async Task<Product> Update(ApplicationDbContext _context, Product p)
         {
             _context.Entry(p).State = EntityState.Modified;
@@ -55,6 +157,59 @@ namespace LumberManagerWebEdition.Data
         {
             return await (from p in _context.Products
                           select p).CountAsync();
+        }
+
+        public static async Task<int> GetTotalProductsAsync(ApplicationDbContext _context, byte height)
+        {
+            return await (from p in _context.Products
+                          where p.Height == height
+                          select p).CountAsync();
+        }
+
+        public static async Task<int> GetTotalProductsAsync(ApplicationDbContext _context, byte height, byte width)
+        {
+            return await (from p in _context.Products
+                          where p.Height == height && p.Width == width
+                          select p).CountAsync();
+        }
+
+        public static async Task<int> GetTotalProductsAsync(ApplicationDbContext _context, byte height, byte width, byte length)
+        {
+            return await (from p in _context.Products
+                          where p.Height == height && p.Width == width && p.Length == length
+                          select p).CountAsync();
+        }
+
+        public static async Task<int> GetTotalProductsAsync(ApplicationDbContext _context, byte height, byte width, byte length, string category)
+        {
+            List<Product> products = await (from p in _context.Products
+                                      where p.Height == height && p.Width == width && p.Length == length
+                                      select p).Include(nameof(Product.Category)).ToListAsync();
+            List<Product> newProducts = new List<Product>();
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Category[0].CategoryName == category)
+                {
+                    newProducts.Add(products[i]);
+                }
+            }
+            return newProducts.Count;
+        }
+
+        public static async Task<int> GetTotalProductsAsync(ApplicationDbContext _context, byte height, byte width, byte length, string category, string treatmentType)
+        {
+            List<Product> products = await (from p in _context.Products
+                                            where p.Height == height && p.Width == width && p.Length == length
+                                            select p).Include(nameof(Product.Category)).ToListAsync();
+            List<Product> newProducts = new List<Product>();
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Category[0].CategoryName == category && products[i].Category[1].CategoryName == treatmentType)
+                {
+                    newProducts.Add(products[i]);
+                }
+            }
+            return newProducts.Count;
         }
 
         public static bool CheckForExistingProduct(ApplicationDbContext _context, Product p)
@@ -75,6 +230,28 @@ namespace LumberManagerWebEdition.Data
                 }
             }
             return false;
+        }
+
+        public static async Task<List<byte>> GetHeightAsync(ApplicationDbContext _context)
+        {
+            List<byte> height =  await (from p in _context.Products
+                          select p.Height).Distinct().ToListAsync();
+            return height;
+        }
+
+        public static async Task<List<byte>> GetWidthAsync(ApplicationDbContext _context, byte height)
+        {
+            List<byte> width = await (from p in _context.Products
+                                      where p.Height == height
+                                      select p.Width).Distinct().ToListAsync();
+            return width;
+        }
+
+        public static async Task<List<byte>> GetLengthAsync(ApplicationDbContext _context, byte height, byte width)
+        {
+            return await (from p in _context.Products
+                          where p.Height == height && p.Width == width
+                          select p.Length).Distinct().ToListAsync();
         }
     }
 }
