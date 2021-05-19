a8dece4c7fe2710fdc9b1c65c393449b0a1f8409
KramerJProg <kramerjensend@gmail.com>
2021-05-19 11:29:00 -0700

    Implemented a shopping cart with cookies.

---
+++ LumberManagerWebEdition/Controllers/CartController.cs
@@ -0,0 +1,34 @@
+﻿using LumberManagerWebEdition.Data;
+using LumberManagerWebEdition.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LumberManagerWebEdition.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpcontext;
+
+        public CartController(ApplicationDbContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpcontext = httpContext;
+        }
+
+        public IActionResult Add(int id, string previousUrl)
+        {
+            Product p = ProductDb.GetProduct(_context, id);
+
+            CookieHelper.AddProductToCart(_httpcontext, p);
+
+            TempData["Message"] = p.Height + " x " + p.Width + " x " + p.Length + " " + p.Category[0] + " " + p.Category[1] + " was added successfully.";
+
+            return Redirect(previousUrl);
+        }
+    }
+}
---
+++ LumberManagerWebEdition/Models/CookieHelper.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LumberManagerWebEdition.Models
+{
+    public class CookieHelper
+    {
+        const string CartCookie = "CartCookie";
+
+        public static List<Product> GetCartProducts(IHttpContextAccessor http)
+        {
+            string existingItems = http.HttpContext.Request.Cookies[CartCookie];
+
+            List<Product> cartProducts = new List<Product>();
+
+            if (existingItems != null)
+            {
+                cartProducts = JsonConvert.DeserializeObject<List<Product>>(existingItems);
+            }
+
+            return cartProducts;
+        }
+
+        public static void AddProductToCart(IHttpContextAccessor http, Product p)
+        {
+            List<Product> cartProducts = GetCartProducts(http);
+
+            cartProducts.Add(p);
+            string data = JsonConvert.SerializeObject(cartProducts, new JsonSerializerSettings() 
+            { 
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddYears(1),
+                Secure = true,
+                IsEssential = true
+            };
+
+            http.HttpContext.Response.Cookies.Append(CartCookie, data, options);
+        }
+
+        public static int GetTotalCartProducts(IHttpContextAccessor http)
+        {
+            List<Product> cartProducts = GetCartProducts(http);
+            return cartProducts.Count;
+        }
+    }
+}
