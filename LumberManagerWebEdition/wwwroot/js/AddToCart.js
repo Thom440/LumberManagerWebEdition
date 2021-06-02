4a3e18910053971a1d73f70e5ba07ca6ee108853
KramerJProg <kramerjensend@gmail.com>
2021-06-01 22:28:27 -0700

    Work in progress js

--- LumberManagerWebEdition/wwwroot/js/AddToCart.js
+++ LumberManagerWebEdition/wwwroot/js/AddToCart.js
@@ -1,6 +1,30 @@
-﻿function changeQuantity(button) {
-    let input = $(button).siblings("#qty").val();
-    let url = $(button).attr("href");
+﻿
+function changeQuantity() {
+    let input = $(this).siblings("#qty").val();
+    let url = $(this).attr("href");
+    if (this.value == "-") {
+        input = input - 1;
+    }
+    else {
+        input = input + 1;
+    }
     url = url.replace("0", input);
-    $(button).attr("href", url);
+    $(this).attr("href", url);
+}
+
+function subtractQuantity() {
+    let input = $(this).siblings("#qty").val();
+    let url = $(this).attr("href");
+    input = input - 1;
+    url = url.replace("0", input);
+    $(this).attr("href", url);
+}
+
+function addQuantity() {
+    let input = $(this).siblings("#qty").val();
+    let num = parseInt(input);
+    let url = $(this).attr("href");
+    num = num + 1;
+    url = url.replace("0", num);
+    $(this).attr("href", url);
 }
