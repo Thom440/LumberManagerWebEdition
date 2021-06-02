748f4c04a0b2aae3ad665a61548a2012e775c5aa
Thom Barrett <72460240+Thom440@users.noreply.github.com>
2021-06-02 14:44:56 -0700

    Issue#89 (#105)
    * Work in progress js
    
    * Able to change product quantities in cart
    
    * Changed ProductCookieHelper to only store productid and quantity to save space
    
    Co-authored-by: KramerJProg <kramerjensend@gmail.com>

--- LumberManagerWebEdition/Models/ProductCookieHelper.cs
+++ LumberManagerWebEdition/Models/ProductCookieHelper.cs
@@ -9,23 +9,6 @@ namespace LumberManagerWebEdition.Models
     {
         public int ProductID { get; set; }
 
-        /// <summary>
-        /// The height of the lumber. ex 2 inches
-        /// </summary>
-        public byte Height { get; set; }
-
-        /// <summary>
-        /// The width of the lumber. ex 4 inches
-        /// </summary>
-        public byte Width { get; set; }
-
-        /// <summary>
-        /// The length of the lumber ex 8 foot
-        /// </summary>
-        public byte Length { get; set; }
-
         public byte Quantity { get; set; }
-
-        public List<Category> Category { get; set; } = new List<Category>();
     }
 }
--- LumberManagerWebEdition/wwwroot/js/AddToCart.js
+++ LumberManagerWebEdition/wwwroot/js/AddToCart.js
@@ -1,6 +1,40 @@
-﻿function changeQuantity(button) {
-    let input = $(button).siblings("#qty").val();
-    let url = $(button).attr("href");
+﻿
+function changeQuantity() {
+    let input = $(this).siblings("#qty").val();
+    let url = $(this).attr("href");
+    if (this.value == "-") {
+        input = input - 1;
+    }
+    else {
+        input = input + 1;
+    }
     url = url.replace("0", input);
-    $(button).attr("href", url);
+    $(this).attr("href", url);
+}
+
+window.onload = function () {
+    let subtract = $(".subtract-button");
+    let add = $(".add-button");
+    add.on("click", addQuantity);
+    subtract.on("click", subtractQuantity)
+}
+
+function subtractQuantity() {
+    console.log(this);
+    console.log($(this).siblings());
+    let input = $(this).siblings("#qty").val();
+    let num = parseInt(input);
+    let url = $(this).attr("formaction");
+    num = num - 1;
+    url = url.replace("0", num);
+    $(this).attr("formaction", url);
+}
+
+function addQuantity() {
+    let input = $(this).siblings("#qty").val();
+    let num = parseInt(input);
+    let url = $(this).attr("href");
+    num = num + 1;
+    url = url.replace("0", num);
+    $(this).attr("href", url);
 }
