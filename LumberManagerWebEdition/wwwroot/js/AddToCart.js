b11e95caca9df8c45f4130b7dd77e9477daaadda
Thom Barrett <thom440@yahoo.com>
2021-06-02 14:43:31 -0700

    Able to change product quantities in cart

--- LumberManagerWebEdition/wwwroot/js/AddToCart.js
+++ LumberManagerWebEdition/wwwroot/js/AddToCart.js
@@ -12,12 +12,22 @@ function changeQuantity() {
     $(this).attr("href", url);
 }
 
+window.onload = function () {
+    let subtract = $(".subtract-button");
+    let add = $(".add-button");
+    add.on("click", addQuantity);
+    subtract.on("click", subtractQuantity)
+}
+
 function subtractQuantity() {
+    console.log(this);
+    console.log($(this).siblings());
     let input = $(this).siblings("#qty").val();
-    let url = $(this).attr("href");
-    input = input - 1;
-    url = url.replace("0", input);
-    $(this).attr("href", url);
+    let num = parseInt(input);
+    let url = $(this).attr("formaction");
+    num = num - 1;
+    url = url.replace("0", num);
+    $(this).attr("formaction", url);
 }
 
 function addQuantity() {
