7526192af2f49c20d57ea87f804ad70c42ef2eb6
Thom Barrett <thom440@yahoo.com>
2021-07-08 20:02:00 -0700

    Added board feet to product. Created new migration for board feet

---
+++ LumberManagerWebEdition/Data/Migrations/20210709030018_BoardFeet.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LumberManagerWebEdition.Data.Migrations
+{
+    public partial class BoardFeet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BoardFeet",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BoardFeet",
+                table: "Products");
+        }
+    }
+}
