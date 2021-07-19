382a4794a80cee4e5c48f04b0ff27beb4f3dec55
Thom Barrett <thom440@yahoo.com>
2021-07-19 11:59:49 -0700

    Changed UserID to a string

--- LumberManagerWebEdition/Data/Migrations/20210719185753_ChangeUserIdToString.Designer.cs
+++ LumberManagerWebEdition/Data/Migrations/20210719185753_ChangeUserIdToString.Designer.cs
@@ -4,14 +4,16 @@ using LumberManagerWebEdition.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LumberManagerWebEdition.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210719185753_ChangeUserIdToString")]
+    partial class ChangeUserIdToString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
