7cccfb55514dd5d9aaf66bbdd7a5498dc326a968
Thom Barrett <thom440@yahoo.com>
2021-07-20 09:51:37 -0700

    Added OrderLineItemsDB class

---
+++ LumberManagerWebEdition/Data/OrderLineItemsDB.cs
@@ -0,0 +1,17 @@
+﻿using LumberManagerWebEdition.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LumberManagerWebEdition.Data
+{
+    public static class OrderLineItemsDB
+    {
+        public static void AddOrderLineItem(ApplicationDbContext _context, OrderLineItems orderLineItems)
+        {
+            _context.OrderLineItems.Add(orderLineItems);
+            _context.SaveChanges();
+        }
+    }
+}
