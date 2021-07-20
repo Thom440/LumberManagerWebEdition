e6bcc2c4037861282ba80da98ba590a2633a07f6
Thom Barrett <thom440@yahoo.com>
2021-07-20 09:54:17 -0700

    Added UserDB class

---
+++ LumberManagerWebEdition/Data/UserDB.cs
@@ -0,0 +1,27 @@
+﻿using LumberManagerWebEdition.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LumberManagerWebEdition.Data
+{
+    public static class UserDB
+    {
+        public static User GetUser(ApplicationDbContext _context, string id)
+        {
+
+            User user = (from u in _context.Customers
+                               where u.UserID == id
+                               select u).SingleOrDefault();
+            return user;
+        }
+
+        public static async Task AddUserAsync(ApplicationDbContext _context, User u)
+        {
+            // Adds user to database
+            _context.Customers.Add(u);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
