88281ba5cf56cb4357413f79789f46099fe55564
KramerJProg <kramerjensend@gmail.com>
2021-07-20 22:30:02 -0700

    Added migration for order line item board footage.

---
+++ LumberManagerWebEdition/Data/Migrations/20210721052847_OrderLineItemsBoardFeet.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LumberManagerWebEdition.Data.Migrations
+{
+    public partial class OrderLineItemsBoardFeet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "PricePer1000BoardFeet",
+                table: "OrderLineItems",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PricePer1000BoardFeet",
+                table: "OrderLineItems");
+        }
+    }
+}
