81e7e7404dd6e174c7e02547e4d73aadf75e8803
KramerJProg <kramerjensend@gmail.com>
2021-07-26 21:33:53 -0700

    Added JavaScript to the Confirm page

---
+++ LumberManagerWebEdition/wwwroot/js/Confirm.js
@@ -0,0 +1,18 @@
+﻿
+window.onload = function() {
+    let submit = document.getElementById("submit");
+    submit.onclick = validateEntries;
+}
+
+function validateEntries() {
+    let isValid = true;
+
+    $("#firstName").blur(function () {
+        if (!$.trim(0).length) {
+            isValid = false;
+            $("#confirm").submit(function (e){
+                e.preventDefault();
+            }
+        }
+    })
+}
