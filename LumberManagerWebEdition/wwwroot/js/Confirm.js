96b5d62e2f537274e413cbaa72763a45ac79ca3b
KramerJProg <kramerjensend@gmail.com>
2021-07-26 22:13:10 -0700

    Added JavaScript to confirm page.

--- LumberManagerWebEdition/wwwroot/js/Confirm.js
+++ LumberManagerWebEdition/wwwroot/js/Confirm.js
@@ -7,12 +7,50 @@ window.onload = function() {
 function validateEntries() {
     let isValid = true;
 
-    $("#firstName").blur(function () {
-        if (!$.trim(0).length) {
-            isValid = false;
-            $("#confirm").submit(function (e){
-                e.preventDefault();
-            })
-        }
-    })
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let address = document.getElementById("address").value;
+    let city = document.getElementById("city").value;
+    let state = document.getElementById("state").value;
+    let zipCode = document.getElementById("zipCode").value;
+
+    if (firstName.trim() == "") {
+        isValid = false;
+        document.getElementById("firstNameError").innerHTML = "First name is required.";
+    }
+
+    if (lastName.trim() == "") {
+        isValid = false;
+        document.getElementById("lastNameError").innerHTML = "Last name is required.";
+    }
+
+    if (address.trim() == "") {
+        isValid = false;
+        document.getElementById("addressError").innerHTML = "Address is required.";
+    }
+
+    if (city.trim() == "") {
+        isValid = false;
+        document.getElementById("cityError").innerHTML = "City is required.";
+    }
+
+    if (state.trim() == "") {
+        isValid = false;
+        document.getElementById("stateError").innerHTML = "State is required.";
+    }
+
+    if (zipCode.trim() == "") {
+        isValid = false;
+        document.getElementById("zipCodeError").innerHTML = "Zip code is required.";
+    }
+
+    if (!isValid) {
+        $("#confirm").submit(function (e) {
+            e.preventDefault();
+        });
+    }
+
+    else {
+        $("#confirm").unbind("submit").submit();
+    }
 }
