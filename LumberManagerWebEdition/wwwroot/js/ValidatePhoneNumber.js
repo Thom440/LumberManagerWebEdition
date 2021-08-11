54020c00cdf87dd440cd30bbf8ce6d899bf6aac7
Thom Barrett <thom440@yahoo.com>
2021-08-10 21:26:25 -0700

    Added phone number formatting validation to the register page

---
+++ LumberManagerWebEdition/wwwroot/js/ValidatePhoneNumber.js
@@ -0,0 +1,27 @@
+﻿window.onload = function () {
+    let submit = document.getElementById("submit-button");
+    submit.onclick = verifyPhoneNumber;
+}
+
+function verifyPhoneNumber() {
+    let phone = document.getElementById("phone");
+    let phoneNumber = phone.value;
+
+    let regex = /^(\+\d{1, 2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}/;
+
+    let isValid = regex.test(phoneNumber);
+
+    if (phoneNumber.trim() == "") {
+        return;
+    }
+
+    if (isValid) {
+        $("#form").unbind("submit").submit();
+    }
+    else {
+        $("#form").submit(function (e) {
+            e.preventDefault();
+        });
+        document.getElementById("phone-span").innerText = "Incorrect Format, ie 253 555 6556 or 253-555-6556";
+    }
+}
